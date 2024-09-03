0574360f06cab130c2614101f6bf0589be5897d7
bradsap54 <115648897+bradsap54@users.noreply.github.com>
2024-09-03 12:19:56 +0700

    update version of each module

--- setup.sh
+++ setup.sh
@@ -78,19 +78,20 @@ while true; do
         sudo docker compose up -d
         ;;
     8)
-        cp wazuh/custom-integrations/custom-iris.py /var/lib/docker/volumes/wazuh_wazuh_integrations/_data/custom-iris.py
+        sudo cp wazuh/custom-integrations/custom-iris.py /var/lib/docker/volumes/wazuh_wazuh_integrations/_data/custom-iris.py
         sudo docker exec -ti wazuh-wazuh.manager-1 chown root:wazuh /var/ossec/integrations/custom-iris.py
         sudo docker exec -ti wazuh-wazuh.manager-1 chmod 750 /var/ossec/integrations/custom-iris.py
-        sudo docker exec -ti wazuh-wazuh.manager-1 apt update -y
-        sudo docker exec -ti wazuh-wazuh.manager-1 apt install python3-pip -y
+        sudo docker exec -ti wazuh-wazuh.manager-1 yum update -y
+        sudo docker exec -ti wazuh-wazuh.manager-1 yum install python3-pip -y
         sudo docker exec -ti wazuh-wazuh.manager-1 pip3 install requests
+
         cd wazuh && sudo docker compose restart
         ;;
     9)
-        cp wazuh/custom-integrations/custom-misp.py /var/lib/docker/volumes/wazuh_wazuh_integrations/_data/custom-misp.py
+        sudo cp wazuh/custom-integrations/custom-misp.py /var/lib/docker/volumes/wazuh_wazuh_integrations/_data/custom-misp.py
         sudo docker exec -ti wazuh-wazuh.manager-1 chown root:wazuh /var/ossec/integrations/custom-misp.py
         sudo docker exec -ti wazuh-wazuh.manager-1 chmod 750 /var/ossec/integrations/custom-misp.py
-        cp wazuh/custom-integrations/local_rules.xml /var/lib/docker/volumes/wazuh_wazuh_etc/_data/rules/local_rules.xml
+        sudo cp wazuh/custom-integrations/local_rules.xml /var/lib/docker/volumes/wazuh_wazuh_etc/_data/rules/local_rules.xml
         sudo docker exec -ti wazuh-wazuh.manager-1 chown wazuh:wazuh /var/ossec/etc/rules/local_rules.xml
         sudo docker exec -ti wazuh-wazuh.manager-1 chmod 550 /var/ossec/etc/rules/local_rules.xml
         cd wazuh && sudo docker compose restart
